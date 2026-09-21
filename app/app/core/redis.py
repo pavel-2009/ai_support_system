c7e7a81d9570e5a8b0880f0f850d323eba913d57
Павел <ms.radionova@bk.ru>
2026-09-21 18:38:29 +0500

    instrumented redis

--- app/app/core/redis.py
+++ app/app/core/redis.py
@@ -1,11 +1,17 @@
 """Redis client creation and dependency provider."""
 
 from redis import Redis
+from opentelemetry.instrumentation.redis import RedisInstrumentor
 
 from app.core.config import settings
 
 
-redis_client = Redis.from_url(settings.REDIS_URL, decode_responses=True)
+RedisInstrumentor().instrument()
+
+redis_client = Redis.from_url(
+    settings.REDIS_URL,
+    decode_responses=True,
+)
 
 
 def get_redis_client() -> Redis:
