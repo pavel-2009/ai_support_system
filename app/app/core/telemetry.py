0ab9963fbb0e5c0a62b0abf63f4eb3a847205379
Павел <ms.radionova@bk.ru>
2026-09-21 18:10:12 +0500

    added telemetry config

---
+++ app/app/core/telemetry.py
@@ -0,0 +1,34 @@
+"""OpenTelemetry configuration"""
+
+from opentelemetry import trace
+from opentelemetry.exporter.otlp.proto.grpc.trace_exporter import OTLPSpanExporter
+from opentelemetry.sdk.resources import Resource
+from opentelemetry.sdk.trace import TracerProvider
+from opentelemetry.sdk.trace.export import BatchSpanProcessor
+
+from app.core.config import settings
+
+
+def configure_telemetry() -> None:
+    """Configure telemetry tracing"""
+
+    resource = Resource.create(
+        {
+            "service.name": settings.OTEL_SERVICE_NAME,
+            "service.version": settings.APP_VERSION,
+            "deployment.environment": settings.OTEL_ENVIRONMENT,
+        }
+    )
+
+    tracer_provider = TracerProvider(resource=resource)
+
+    exporter = OTLPSpanExporter(
+        endpoint=settings.OTEL_EXPORTER_OTLP_ENDPOINT,
+        insecure=settings.OTEL_EXPORTER_OTLP_INSECURE
+    )
+
+    span_processor = BatchSpanProcessor(exporter)
+
+    tracer_provider.add_span_processor(span_processor)
+
+    trace.set_tracer_provider(tracer_provider)
